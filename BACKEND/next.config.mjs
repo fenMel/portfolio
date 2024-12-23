160d41bfa7152c6d9574bfbac862b126bec001ef
MELISSA FENZI <femelissa@etudiant-esic.fr>
2024-12-24 00:44:34 +0100

    Update next.config.mjs

--- BACKEND/next.config.mjs
+++ BACKEND/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.devtool = 'source-map'; // Active les source maps uniquement en développement
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
